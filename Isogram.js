332648fa6c4286564c8fb300336804bfce93163f
IrynaKrt <rinakashi13@mail.ru>
2022-08-23 16:22:10 +0300

    Isogram <7kyu>

---
+++ Isogram.js
@@ -0,0 +1,18 @@
+// This is my first solution
+function isIsogram(str){
+    return !str.match(/([a-z])?!.*\1/ig) 
+}
+  
+  
+// But this is faster and easier to understand 
+function isIsogram(str){
+    str = str.toLowerCase();
+    for(let i = 0; i < str.length; i++) {
+      for(let j = i + 1; j < str.length; j++) {
+        if(str[i] === str[j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+}
