8b1d289cf438ca2e0e445abe081fbd813982d370
IrynaKrt <106380034+IrynaKrt@users.noreply.github.com>
2023-01-12 20:00:32 +0300

    Even or Odd <8kyu>

---
+++ Even_or_Odd.js
@@ -0,0 +1,11 @@
+//Solution 1
+function evenOrOdd(number) {return number % 2 === 0? "Even": "Odd";}
+
+//Solution 2
+function evenOrOdd(number) {
+  if(number % 2 === 0) {
+      return "Even";
+  } else {
+      return "Odd";
+  }
+}
